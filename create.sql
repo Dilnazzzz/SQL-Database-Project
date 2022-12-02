01ada654e539130e44aea1a3ecc1244720ae8255
Dilnaz Baltabayeva <76237763+Dilnazzzz@users.noreply.github.com>
2022-12-02 11:31:17 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -26,23 +26,38 @@ CREATE TABLE SALES (
     FOREIGN KEY (AGENTNAME) REFERENCES HOUSES(AGENTNAME)
 );
 
-INSERT INTO HOUSES VALUES (1, 'Summer Gallegos', 6, 1, 2553500, 81231, '2020-06-24', 'Karen Cisneros', 'New York', 'Not Sold'); 
-INSERT INTO HOUSES VALUES (2, 'Liam Hood', 4, 1, 1715212, 74420, '2020-08-23', 'Dillon Mcclure', 'Los Angeles', 'Not Sold');
-INSERT INTO HOUSES VALUES (3, 'Enzo Burgess', 5, 3, 2532682, 19424, '2020-09-26', 'Seth Mcleod', 'Los Angeles', 'Not Sold');
-INSERT INTO HOUSES VALUES (4, 'Mackenzie Lambert', 5, 3, 2421988, 60784, '2020-05-10', 'Liam Hanson', 'New York', 'Not Sold');
-INSERT INTO HOUSES VALUES (5, 'Danielle Bowers', 2, 2, 131126, 46878, '2020-06-01', 'Verity Morrow', 'New York', 'Not Sold');
-INSERT INTO HOUSES VALUES (6, 'Elsa Collier', 7, 1, 1256833, 65969, '2021-07-27', 'Ishaq Matthams', 'Chicago', 'Not Sold');
-INSERT INTO HOUSES VALUES (7, 'Samira Burns', 1, 2, 2258594, 75485, '2021-10-23', 'Liam Hanson', 'New York', 'Not Sold');
-INSERT INTO HOUSES VALUES (8, 'Hector Galvan', 4, 3, 1010053, 49853, '2021-07-01', 'Seth Mcleod', 'Houston', 'Not Sold');
-INSERT INTO HOUSES VALUES (9, 'Katy Mcintosh', 1, 1, 2009602, 56207, '2021-04-13', 'Karen Cisneros', 'New York', 'Not Sold');
-INSERT INTO HOUSES VALUES (10, 'Miles Raymond', 7, 3, 2338257, 60963, '2021-12-13'), 'Conor Wood', 'Chicago', 'Not Sold')
+INSERT INTO HOUSES VALUES (1, 'Summer Gallegos', 6, 1, 1553000, 81231, '2020-01-04', 'Karen Cisneros', 'New York', 'Not Sold'); 
+INSERT INTO HOUSES VALUES (2, 'Liam Hood', 4, 1, 715000, 74420, '2020-01-04', 'Dillon Mcclure', 'Los Angeles', 'Not Sold');
+INSERT INTO HOUSES VALUES (3, 'Enzo Burgess', 5, 3, 532000, 19424, '2020-01-06', 'Seth Mcleod', 'Phoenix', 'Not Sold');
+INSERT INTO HOUSES VALUES (4, 'Mackenzie Lambert', 5, 3, 1421000, 60784, '2020-01-10', 'Liam Hanson', 'New York', 'Not Sold');
+INSERT INTO HOUSES VALUES (5, 'Danielle Bowers', 2, 2, 131000, 46878, '2020-01-11', 'Verity Morrow', 'San Jose', 'Not Sold');
+INSERT INTO HOUSES VALUES (6, 'Elsa Collier', 7, 1, 1256000, 65969, '2020-01-12', 'Ishaq Matthams', 'Chicago', 'Not Sold');
+INSERT INTO HOUSES VALUES (7, 'Samira Burns', 1, 2, 258000, 75485, '2020-01-13', 'Liam Hanson', 'New York', 'Not Sold');
+INSERT INTO HOUSES VALUES (8, 'Hector Galvan', 4, 3, 1010000, 49853, '2020-01-13', 'Seth Mcleod', 'Houston', 'Not Sold');
+INSERT INTO HOUSES VALUES (9, 'Katy Mcintosh', 1, 1, 609000, 56207, '2020-01-13', 'Karen Cisneros', 'San Diego', 'Not Sold');
+INSERT INTO HOUSES VALUES (10, 'Miles Raymond', 7, 3, 338000, 60963, '2020-01-13', 'Conor Wood', 'Chicago', 'Not Sold')
+INSERT INTO HOUSES VALUES (11, 'Zainab Garcia', 5, 2, 315000, 80246, '2020-01-16', 'Karen Cisneros', 'Houston', 'Not Sold');
+INSERT INTO HOUSES VALUES (12, 'Marilyn Vance', 1, 1, 76000, 25949, '2020-01-17', 'Ishaq Matthams', 'New York', 'Not Sold');
+INSERT INTO HOUSES VALUES (13, 'Valentina Craig', 4, 3, 248000, 19272, '2020-01-17', 'Dillon Mcclure', 'Philadelphia', 'Not Sold');
+INSERT INTO HOUSES VALUES (14, 'Subhaan Mcmahon', 2, 2, 488000, 52671, '2020-01-22', 'Seth Mcleod', 'Chicago', 'Not Sold');
+INSERT INTO HOUSES VALUES (15, 'Alfie Schneider', 6, 3, 563000, 11658, '2020-01-23', 'Dillon Mcclure', 'Los Angeles', 'Not Sold');
+INSERT INTO HOUSES VALUES (16, 'Alexandros Maldonado', 5, 3, 200000, 49968, '2020-01-23', 'Liam Hanson', 'Phoenix', 'Not Sold');
+INSERT INTO HOUSES VALUES (17, 'Hasnain Love', 7, 1, 845000, 44649, '2020-01-27', 'Cameron Ford', 'Dallas', 'Not Sold');
+INSERT INTO HOUSES VALUES (18, 'Elspeth Case', 5, 1, 595000, 25135, '2020-01-27', 'Liam Hanson', 'Los Angeles', 'Not Sold');
+INSERT INTO HOUSES VALUES (19, 'Dewi Howard', 4, 2, 756000, 18552, '2020-01-31', 'Karen Cisneros', 'San Antonio', 'Not Sold');
+INSERT INTO HOUSES VALUES (20, 'Sasha Little', 2, 3, 806000, 81638, '2020-01-31', 'Verity Morrow', 'Chicago', 'Not Sold');
 
-
-INSERT INTO SALES VALUES (10, 1, 'Lacie Rollins', 250000, 'Karen Cisneros', '2022-11-05', 'New York');
-INSERT INTO SALES VALUES (11, 3, 'Alice Christian', 450000, 'Seth Mcleod', '2022-11-05', 'New York');
-INSERT INTO SALES VALUES (12, 6, 'Alice Christian', 450000, 'Ishaq Matthams', '2022-11-05', 'San Francisco');
-INSERT INTO SALES VALUES (13, 10, 'Alice Christian', 450000, 'Conor Wood', '2022-11-05', 'San Francisco');
-INSERT INTO SALES VALUES (14, 2, 'Alice Christian', 450000, 'Dillon Mcclure', '2022-11-05', 'San Francisco');
+INSERT INTO SALES VALUES (10, 1, 'Lacie Rollins', 1153000, 'Karen Cisneros', '2020-04-05', 'New York');
+INSERT INTO SALES VALUES (11, 3, 'Alice Christian', 632000, 'Seth Mcleod', '2020-04-05', 'Phoenix');
+INSERT INTO SALES VALUES (12, 6, 'Neave Gregory', 1356000, 'Ishaq Matthams', '2020-04-07', 'Chicago');
+INSERT INTO SALES VALUES (13, 10, 'Amelia Archer', 438000, 'Conor Wood', '2020-04-09', 'Chicago');
+INSERT INTO SALES VALUES (14, 12, 'Jorge Gilmore', 96000, 'Ishaq Matthams', '2020-04-15', 'New York');
+INSERT INTO SALES VALUES (15, 13, 'Ameer Watson', 348000, 'Dillon Mcclure', '2020-04-15', 'Philadelphia');
+INSERT INTO SALES VALUES (16, 14, 'Sana Berger', 588000, 'Seth Mcleod', '2020-04-16', 'Chicago');
+INSERT INTO SALES VALUES (17, 15, 'Hajra Middleton', 663000, 'Dillon Mcclure', '2020-04-17', 'Los Angeles');
+INSERT INTO SALES VALUES (18, 17, 'Drew Boyd', 945000, 'Cameron Ford', '2020-04-18', 'Dallas');
+INSERT INTO SALES VALUES (19, 19, 'Omar Bowers', 456000, 'Karen Cisneros', '2020-04-18', 'San Antonio');
+INSERT INTO SALES VALUES (20, 20, 'Marwa Hardy', 506000, 'Verity Morrow', '2020-04-25', 'Chicago');
 
 SELECT '';
 SELECT "Find the top 5 offices with the most sales for that month";
